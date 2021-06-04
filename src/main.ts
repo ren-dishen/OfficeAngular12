774f5cce506ca84c2f85da5e8324ad316d0d097e
Michael Grigoriev <m.grigoriev@busylamp.com>
2021-06-04 12:44:13 +0300

    Manifests

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'office-js';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+Office.initialize = ()  => {
+  console.log("test")
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+};
